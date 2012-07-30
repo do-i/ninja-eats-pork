47668a758454fda03e350e2a9609b6e0ef4e83cf
Alaskan Ninja <ala.nin@ninja.net>
2012-07-30 00:26:01 -0500

    original ssh key

--- Ninja.java
+++ Ninja.java
@@ -1,13 +1,3 @@
 public class Ninja{
-
-	String str="shhhhhh";
-	int i=0;
-	double ddd;
-public void getStr(){
-str=str.toUpperCase();
-if(null == str){
-return "";
-}
-return str;
-}
+// nothing to hide
 }
